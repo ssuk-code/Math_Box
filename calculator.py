f263dba6806978195fe3309d45625edda711bb5f
ssuk <ssukcode@gmail.com>
2021-11-24 21:38:56 +0900

    Add get_Sum_ver2 function

--- calculator.py
+++ calculator.py
@@ -26,5 +26,9 @@ def get_Abs(num):
 def getPercent(a, b):
     return (a / b) * 100
 
-def get_Sum_ver1(n):
-	return n(n + 1) / 2
+def get_Sum_ver2(n):
+	sum = 0
+	for i in range(1, n+1):
+		sum = sum + i
+
+	return sum
